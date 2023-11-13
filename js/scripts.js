fef481308d92cafea91c2876a8311b5980f4f0b0
savsalmon <savannah.b.hilton@gmail.com>
2023-11-13 02:37:13 -0700

    added js for phot buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+let datingBtn = document.querySelector(".dating")
+let engagedBtn = document.querySelector(".engaged")
+let engagementBtn = document.querySelector(".engagements")
+let bridalsBtn = document.querySelector(".bridals")
+let weddingBtn = document.querySelector(".wedding")
+let allPhotosBtn = document.querySelector(".all")
+let photosDisplay = document.querySelector(".photosDisplay")
+
 /* NAV open and close */
 /* Open */
 function openNav() {
@@ -55,33 +63,34 @@ function addSpace(x) {
   return x + " ♥ "
 }
 
-const text = document.querySelector(".circleText")
-const textContent = addSpace(text.innerText.trim())
-const numChars = textContent.length
-const radius = 100 // Adjust this value as needed for the circle size
+function createTextCircle() {
+  const text = document.querySelector(".circleText")
+  if (text) {
+    const textContent = addSpace(text.innerText.trim())
+    const numChars = textContent.length
+    const radius = 100 // Adjust this value as needed for the circle size
 
-text.innerHTML = textContent
-  .split("")
-  .map((char, i) => {
-    const angle = (360 / numChars) * i
-    const x = radius * Math.cos((angle * Math.PI) / 180)
-    const y = radius * Math.sin((angle * Math.PI) / 180)
+    text.innerHTML = textContent
+      .split("")
+      .map((char, i) => {
+        const angle = (360 / numChars) * i
+        const x = radius * Math.cos((angle * Math.PI) / 180)
+        const y = radius * Math.sin((angle * Math.PI) / 180)
 
-    // Adjusting the positioning to the center of the circle
-    const translationX = x - 10 // Adjust this value to fine-tune the positioning
-    const translationY = y - 10 // Adjust this value to fine-tune the positioning
+        // Adjusting the positioning to the center of the circle
+        const translationX = x - 10 // Adjust this value to fine-tune the positioning
+        const translationY = y - 10 // Adjust this value to fine-tune the positioning
 
-    return `<span style="position: absolute; transform: translate(${translationX}px, ${translationY}px) rotate(${
-      angle + 90
-    }deg);">${char}</span>`
-  })
-  .join("")
+        return `<span style="position: absolute; transform: translate(${translationX}px, ${translationY}px) rotate(${
+          angle + 90
+        }deg);">${char}</span>`
+      })
+      .join("")
+  }
+}
 
-let engagementBtn = document.querySelector(".engagements")
-let bridalsBtn = document.querySelector(".bridals")
-let weddingBtn = document.querySelector(".wedding")
-let allPhotosBtn = document.querySelector(".all")
-let photosDisplay = document.querySelector(".photosDisplay")
+// Call the function to create the text circle
+createTextCircle()
 
 function openNav() {
   document.getElementById("myNav").style.width = "100%"
@@ -103,33 +112,53 @@ function checkScreenWidth() {
   }
 }
 
+datingBtn.addEventListener("click", handleDatingClick)
+engagedBtn.addEventListener("click", handleEngagedClick)
+bridalsBtn.addEventListener("click", handleBridalsClick)
 engagementBtn.addEventListener("click", handleEngagementClick)
 bridalsBtn.addEventListener("click", handleBridalsClick)
 weddingBtn.addEventListener("click", handleWeddingClick)
 allPhotosBtn.addEventListener("click", handleAllPhotosClick)
 
+function handleDatingClick() {
+  photosDisplay.innerHTML = ""
+  let message = document.createElement("h2")
+  message.textContent =
+    "We'll be adding the photos of our early dating soon! Please check back later."
+  photosDisplay.appendChild(message)
+}
+
+function handleEngagedClick() {
+  photosDisplay.innerHTML = ""
+  let message = document.createElement("h2")
+  message.textContent =
+    "We'll be adding the photos of our dating while engaged photos soon! Please check back later."
+  photosDisplay.appendChild(message)
+}
+
 function handleEngagementClick() {
   photosDisplay.innerHTML = ""
-  for (let index = 1; index <= 166; index++) {
+  for (let index = 1; index <= 112; index++) {
     let newImg = document.createElement("img")
-    newImg.src = `images/daphne-ben-engagements/engagements${index}.jpg`
+    newImg.src = `images/Engagements/Savannah+Preston_engagements-${index}.jpg`
     photosDisplay.appendChild(newImg)
   }
 }
 
 function handleBridalsClick() {
   photosDisplay.innerHTML = ""
-  let message = document.createElement("h2")
-  message.textContent =
-    "Sorry! We don't have the Bridal pictures available yet. Please check back later."
-  photosDisplay.appendChild(message)
+  for (let index = 1; index <= 140; index++) {
+    let newImg = document.createElement("img")
+    newImg.src = `images/Bridals/PrestoSavBridals${index}.jpg`
+    photosDisplay.appendChild(newImg)
+  }
 }
 
 function handleWeddingClick() {
   photosDisplay.innerHTML = ""
   let message = document.createElement("h2")
   message.textContent =
-    "Sorry! We don't have the Wedding pictures available yet, especially since the wedding doesn't take place till 11/17/2023. Please check back later."
+    "Sorry! We don't have the Wedding pictures available yet, especially since the wedding doesn't take place till 12/16/2023. Please check back later."
   photosDisplay.appendChild(message)
 }
 
@@ -137,7 +166,8 @@ function handleAllPhotosClick() {
   photosDisplay.innerHTML = ""
   let message = document.createElement("h2")
   message.textContent =
-    "Sorry! We only have the Engagement pictures so far, but here they all are!"
+    "Sorry! We only have the Engagement Photoshoot & Bridal Photoshoot pictures so far, but here they all are!"
   handleEngagementClick()
+  handleBridalsClick()
   photosDisplay.appendChild(message)
 }
